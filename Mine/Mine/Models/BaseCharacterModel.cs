6db053ff71c7d16988ff94a7b057c85f95c34186
Joel Davidson <davids24@seattleu.edu>
2020-02-09 12:44:31 -0800

    Note: I have no clue what is being changed in the 33 other files every time I commit. I will email Dr. Koenig and see if he knows what these are.
    In this commit I further delineated the roles of the PlayerCharacter class vs its parent, the BaseCharacterModel. I implemented no functionality, but the playerCharacter class should now have all of the required datastructures and methods to perform its role. I added default returns for each method just to reach compilation.

--- Mine/Mine/Models/BaseCharacterModel.cs
+++ Mine/Mine/Models/BaseCharacterModel.cs
@@ -20,14 +20,6 @@ namespace PrimeAssault.Models
         bool dead;
         String ImageURI;
         int Level;
-        int experienceTotal;
-        ItemModel Head;
-        ItemModel Feet;
-        ItemModel Arms;
-        ItemModel RightHand;
-        ItemModel LeftHand;
-        ItemModel LeftFinger;
-        ItemModel RightFinger;
         String Ability;
         //Move[2] moveSet; --- Each character will have an array of 2 moves from which they can use attacks in battle. However, these moves structs must be designed and implemented.
 
@@ -40,7 +32,7 @@ namespace PrimeAssault.Models
             Bool ScaleLevel(int level) //Scale the character to the new level
             Bool LevelUp() //Levels up the character if they are ready
             Int levelUpToValue(int value) //Force leveling up to a level, say start a new character at level 5
-            Bool AddExperience(int experience) // Add experience to current character
+            
             Bool TakeDamage(int) //Character receives damage
 
             Int GetAttack()   //Return the calculated attack
@@ -52,14 +44,10 @@ namespace PrimeAssault.Models
             Int GetDamageRollValue()  //Get the calculated damage that this weapon rolled
 
             List<ItemModel> DropAllItems()  //Set of items the character drops when dead
-            ItemModel RemoveItem(Location)  //Remove Item from location
-            ItemModel GetItemByLocation(item) //Get Item info from Location
-            ItemModel AddItem(Location,Item)  //Add item to location
-            Int GetItemBonus(attribute)  //Get all the bonuses for the attribute
+            
 
          */
-
-
+      
 
     }
 }
---
+++ Mine/Mine/Models/PlayerCharacter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeAssault.Models
+{
+    class PlayerCharacter : BaseCharacterModel
+    {
+        int experienceTotal;
+        ItemModel Head;
+        ItemModel Arms;
+        ItemModel RightHand;
+        ItemModel RightFinger;
+        ItemModel LeftHand;
+        ItemModel LeftFinger;
+        ItemModel Feet;
+
+        ItemModel[] EquippedArray = new ItemModel[7]; //this array will (for the sake of convenience, contain all the equipment in the order presented above (Head[0], Arms[1], RightHand[2], RightFinger[3], LeftHand[4], LeftFinger[5], Feet[6])
+
+        bool AddExperience(int experience) // Add experience to current character
+        {
+            return true;
+        }
+        ItemModel RemoveItem(string location)  //Remove Item from location
+        {
+            return Head;
+        }
+        ItemModel GetItemByLocation(string location) //Get Item info from Location
+        {
+            return Head;
+        }
+        void AddItem(string location, ItemModel item)  //Add item to location
+        {
+            
+        }
+        int GetItemBonus(string attribute)  //Get all the bonuses for the attribute
+        {
+            return 2;
+        }
+    }
+}
