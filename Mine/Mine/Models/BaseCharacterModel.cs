f876733684012cd9e7e79926f61641b7a2e16b7c
Joel Davidson <davids24@seattleu.edu>
2020-02-09 18:38:32 -0800

    Created framework for baseCharacterModel class with no implementations. Also changed move struct to be a move class.

--- Mine/Mine/Models/BaseCharacterModel.cs
+++ Mine/Mine/Models/BaseCharacterModel.cs
@@ -6,8 +6,8 @@ namespace PrimeAssault.Models
 {
     class BaseCharacterModel : BaseModel
     {
-        int NUM_MOVES = 2;
-        struct move //we will eventually need to create another CRUDi for this
+        const int NUM_MOVES = 2;
+        class move //we will eventually need to create another CRUDi for this
         {
             string type; //as in ranged or close or heal
             string name; 
@@ -15,8 +15,7 @@ namespace PrimeAssault.Models
             uint uses; // number of allowed uses per dungeon sequence
         }
 
-        move[] moveSet = new move[2]; //--- Each character will have an array of 2 moves from which they can use attacks in battle.
-
+        move[] moveSet = new move[NUM_MOVES]; //--- Each character will have an array of 2 moves from which they can use attacks in battle.
 
         int level;
         int health;
@@ -33,33 +32,82 @@ namespace PrimeAssault.Models
         string imageURI;
         string ability;
         
-       
+        BaseCharacterModel() // default constructor which populates with the bare minimum stats any character can have
+        {
+            level = 1;
+            health = 10;//
+            speed = 1;
+            defense = 1;
+            rangedDefense = 0;
+            defense = 0;
 
-
-        /*Required Methods
-            Character() // default constructor
-            Character() // Make a copy of a character constructor
-            CharacterUpdate() //Update this with each attribute passed in
-            String FormatOutput() //Text output of the class, a fancy toString()
-            Bool ScaleLevel(int level) //Scale the character to the new level
-            Bool LevelUp() //Levels up the character if they are ready
-            Int levelUpToValue(int value) //Force leveling up to a level, say start a new character at level 5
+            for (int i = 0; i < NUM_MOVES; ++i)
+                moveSet[i] = null;
             
-            Bool TakeDamage(int) //Character receives damage
+        }
+        void CharacterUpdate() //Update this with each attribute passed in
+        {
 
-            Int GetAttack()   //Return the calculated attack
-            Int GetSpeed()
-            Int GetDefense()
-            Int GetMaxHealth()
-            Int GetCurrentHealth()  //Return the calculated current health
-            Int GetDamageDice()   //Get the dice to roll for the weapon used
-            Int GetDamageRollValue()  //Get the calculated damage that this weapon rolled
+        }
 
-            List<ItemModel> DropAllItems()  //Set of items the character drops when dead
+        string FormatOutput() //Text output of the class, a fancy toString()
+        {
+            return "";
+        }
+        bool ScaleLevel(int level) //Scale the character to the new level
+        {
+            return true;
+        }
+        bool LevelUp() //Levels up the character if they are ready
+        {
+            return true;
+        }
+        int levelUpToValue(int value) //Force leveling up to a level, say start a new character at level 5
+        {
+            return 0;
+        }
             
+        bool TakeDamage(int attackVal) //Character receives damage
+        {
+            return true;
+        }
 
-         */
-      
+        int GetAttack()   //Return the calculated attack
+        {
+            return 0;
+        }
+        int GetSpeed()
+        {
+            return 0;
+        }
+        int GetDefense()
+        {
+            return 0;
+        }
+        int GetMaxHealth()
+        {
+            return 0;
+        }
+        int GetCurrentHealth()  //Return the calculated current health
+        {
+            return 0;
+        }
+        int GetDamageDice()   //Get the dice to roll for the weapon used
+        {
+            return 0;
+        }
+        int GetDamageRollValue()  //Get the calculated damage that this weapon rolled
+        {
+            return 0;
+        }
+
+        List<ItemModel> DropAllItems()  //Set of items the character drops when dead
+        {
+            List<ItemModel> droppedItems = new List<ItemModel>();
+            //insert code here
+
+            return droppedItems;
+        }
 
     }
 }
