78f3c45a0ddb33142c25843cdcab96b0bccb348e
Blake Werner <wernerblake@seattleu.edu>
2020-02-11 17:33:33 -0800

    Created a View Model page for CharIndex

---
+++ Mine/Mine/ViewModels/CharIndexViewModel.cs
@@ -0,0 +1,27 @@
+﻿using PrimeAssault.Services;
+using PrimeAssault.Models;
+using PrimeAssault.Views;
+using System.Diagnostics;
+using System;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using System.Linq;
+
+namespace PrimeAssault.ViewModels
+{
+    public class CharIndexViewModel : BaseViewModel
+    {
+
+
+        public CharIndexViewModel()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
