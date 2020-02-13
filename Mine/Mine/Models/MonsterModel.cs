e64a656bca2f5ccbfc39f9906a7282a5ea36c425
Joel Davidson <davids24@seattleu.edu>
2020-02-13 12:35:24 -0800

    Added sewere gator as default info and removed all classes in monster that were vestigal from the other character class. Minor code tweaks included.

--- Mine/Mine/Models/MonsterModel.cs
+++ Mine/Mine/Models/MonsterModel.cs
@@ -6,38 +6,24 @@ namespace PrimeAssault.Models
 {
     public class MonsterModel : BaseCharacterModel
     {
-        const int NUM_SLOTS = 7;
-
-        //current level up equation
-        //(BASE_XP + (LVL_MULT * level) ^ LVL_EXP) = (100 + (5 * level) ^ 2)
-        const int BASE_XP = 100;
-        const int LVL_MULT = 5;
+        //current xp give equation
+        //(BASE_XP + (LVL_MULT * level) ^ LVL_EXP) = (1 + (2 * level) ^ 2)
+        const int BASE_XP = 1;
+        const int LVL_MULT = 2;
         const int LVL_EXP = 2;
 
-        const string DEFAULT_CLASS = "soldier";
+        const string DEFAULT_URI = "sewer_gator.png";
 
         public uint experienceTotal { get; set; } = 0;
-        public uint nextLevel { get; set; } = 0;
-        public double nextLevelMultiplier { get; set; } = 0;
-        public string jobClass { get; set; } = DEFAULT_CLASS;
         
-        /*
-        ItemModel Head;
-        ItemModel Arms;
-        ItemModel RightHand;
-        ItemModel RightFinger;
-        ItemModel LeftHand;
-        ItemModel LeftFinger;
-        ItemModel Feet;
-        */
-        ItemModel[] EquippedArray = new ItemModel[NUM_SLOTS]; //this array will (for the sake of convenience, contain all the equipment in the order presented above (Head[0], Arms[1], RightHand[2], RightFinger[3], LeftHand[4], LeftFinger[5], Feet[6])
+        ItemModel Drop = new ItemModel(); //should be retrieved at random?
         
         public int Value { get; set; } = 0;
+
         public bool Update(MonsterModel data)
         {
             Name = data.Name;
             Description = data.Description;
-            jobClass = data.jobClass;
             level = data.level;
             health = data.health;
             maxHealth = data.maxHealth;
@@ -57,58 +43,9 @@ namespace PrimeAssault.Models
             return true;
         }
 
-        enum jobList //might use in the future to make more tenable code that looks more like Dr. Koenig's, keeping as reminder
-        {
-            Soldier,
-            Hunter,
-            Brawler,
-            Mechanist,
-            Mechanic,
-            Ringleader
-        }
-
         public MonsterModel() : base()
         {
-            nextLevel = 100;
-            
-            for (int i = 0; i < NUM_SLOTS; ++i)
-                EquippedArray[i] = null;
-
-            jobClass = DEFAULT_CLASS;
-            updateClass();
-        }
-        
-        bool AddExperience(uint experience) // Add experience to current Monster
-        {
-            experienceTotal += experience;
-            LevelUp();
-            return true;
-        }
-
-        ItemModel RemoveItem(string location)  //Remove Item from location by setting old location to null. Returns the old item.
-        {
-            int index = translateLocationNameToArrayLocation(location);
-            ItemModel RemovedItem = EquippedArray[index];
-            EquippedArray[index] = null;
-            return RemovedItem;
-        }
-
-        ItemModel GetItemByLocation(string location) //Get Item info from Location
-        {
-            int index = translateLocationNameToArrayLocation(location);
-            ItemModel ExamineItem = EquippedArray[index];
-            return ExamineItem;
-        }
-
-        bool AddItem(string location, ItemModel item)  //Add item to location
-        {
-            if (GetItemByLocation(location) == null)
-            {
-                int index = translateLocationNameToArrayLocation(location);
-                EquippedArray[index] = item;
-                return true;
-            }
-            return false;
+            imageURI = "sewer_monster.png";
         }
 
         int GetItemBonus()  //Get all the bonuses for the attributes (should be incorporated after items)
@@ -118,14 +55,8 @@ namespace PrimeAssault.Models
 
         bool LevelUp() //Levels up the Monster if they are ready
         {
-            if (experienceTotal >= (nextLevel))
-            {
-                ++level;
-                increaseStats();
-                nextLevel += (uint)((BASE_XP + (LVL_MULT * level) ^ LVL_EXP) * nextLevelMultiplier);
-                return true;
-            }
-            return false;
+            level++;
+            return true;
         }
 
         void increaseStats() //Basic level up stat increases, temporary, will eventually change when classes are better hammered out
@@ -135,27 +66,6 @@ namespace PrimeAssault.Models
             rangedDefense += 2;
             speed += 2;
             attack += 2;
-        }
-
-        int translateLocationNameToArrayLocation(string location) //Helper function which maps equip locations to array indices
-        {
-            location = location.ToLower();
-            if (location == "head")
-                return 0;
-            else if (location == "arms")
-                return 1;
-            else if (location == "righthand")
-                return 2;
-            else if (location == "rightfinger")
-                return 3;
-            else if (location == "lefthand")
-                return 4;
-            else if (location == "leftfinger")
-                return 5;
-            else if (location == "feet")
-                return 6;
-            else
-                return -1;
         }
 
         void MonsterUpdate(MonsterModel data)
@@ -168,8 +78,7 @@ namespace PrimeAssault.Models
             attack = data.attack;
             Description = data.Description;
             Name = data.Name;
-            jobClass = data.jobClass.ToLower();
-            updateClass();
+            level = data.level;
         }
 
         void levelMultipliers()
@@ -181,93 +90,6 @@ namespace PrimeAssault.Models
             attackMult = 1;
         }
 
-        bool updateClass()//ugly awful class, not maintanable, needs work. So sorry to everyone, just trying to get class up on its feet. Hardcode galore.
-        {
-            if (jobClass == "soldier") //helper class with default values for each class
-            {
-                levelMultipliers();
-                healthMult += .2;
-                speedMult += -.1;
-                defenseMult += .3;
-                rangedDefenseMult += -.3;
-                attackMult += .2;
-                Description = "Soldiers have high health, and defense, and are more likely to get abilities that help with close-range combat.";
-                jobClass = "Soldier";
-                nextLevelMultiplier = 1.0;
-                imageURI = "soldier_class.png";
-                return true;
-            }
-            if (jobClass == "hunter")
-            {
-                levelMultipliers();
-                healthMult += -.3;
-                speedMult += .3;
-                defenseMult += 0;
-                rangedDefenseMult += .15;
-                attackMult += .15;
-                Description = "The hunter has high Attack, and Speed and its bonuses usually lend to killing sewer creatures from afar.";
-                jobClass = "Hunter";
-                nextLevelMultiplier = 1.0;
-                imageURI = "hunter_class.png";
-                return true;
-            }
-            if (jobClass == "brawler")
-            {
-                levelMultipliers();
-                healthMult += .5;
-                speedMult += -.5;
-                defenseMult += .15;
-                rangedDefenseMult += .15;
-                attackMult += .05;
-                Description = "The brawler is a beef-cake with high overall survivability, but no amazing offensive power.";
-                jobClass = "Brawler";
-                nextLevelMultiplier = 1.1;
-                imageURI = "brawler_class.png";
-                return true;
-            }
-            if (jobClass == "mechanic")
-            {
-                levelMultipliers();
-                healthMult += -.2;
-                speedMult += .4;
-                defenseMult += -.2;
-                rangedDefenseMult += .2;
-                attackMult += -.25;
-                Description = "Mechanic has low overall stats, but all mechanic abilities allow for healing of teammates.";
-                jobClass = "Mechanic";
-                nextLevelMultiplier = 1.2;
-                imageURI = "mechanic_class.png";
-                return true;
-            }
-            if (jobClass == "mechanist")
-            {
-                levelMultipliers();
-                healthMult += 0;
-                speedMult += 0;
-                defenseMult += .1;
-                rangedDefenseMult += .1;
-                attackMult += .1;
-                Description = "The mechanist has high ranged defense and attack, and its bonuses are good for killing Euphrates mechs.";
-                jobClass = "Mechanist";
-                nextLevelMultiplier = 1.1;
-                imageURI = "mechanist_class.png";
-                return true;
-            }
-            if (jobClass == "ringleader")
-            {
-                levelMultipliers();
-                healthMult += -.1;
-                speedMult += -.1;
-                defenseMult += -.1;
-                rangedDefenseMult += -.1;
-                attackMult += -.1;
-                Description = "A ringleader has low base stats, but compensates for this through its ability to channel 10 rings to great possible effect.";
-                jobClass = "Ringleader";
-                nextLevelMultiplier = 1.3;
-                imageURI = "ringleader_class.png";
-                return true;
-            }
-            return false;
-        }
+        
     }
 }
