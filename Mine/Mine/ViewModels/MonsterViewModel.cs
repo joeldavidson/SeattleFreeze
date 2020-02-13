c84ac6a5de72d90b0d216acd369829e063d341bf
Blake Werner <wernerblake@seattleu.edu>
2020-02-12 18:21:04 -0800

    Copied and altered View Models to support MonsterView and MonsterIndexView

---
+++ Mine/Mine/ViewModels/MonsterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PrimeAssault.Models;
+
+namespace PrimeAssault.ViewModels
+{
+    public class MonsterViewModel : BaseViewModel
+    {
+        public MonsterModel Data { get; set; }
+
+        public MonsterViewModel(MonsterModel data = null)
+        {
+            Title = data?.Name;
+            Data = data;
+        }
+    }
+}
