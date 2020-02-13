2923798fa393a5f95db8770ebf19a1f860c44034
Blake Werner <wernerblake@seattleu.edu>
2020-02-12 18:18:21 -0800

    Created Monster file by copying Character file and did a mass name change to

---
+++ Mine/Mine/Views/Monster/MonIndexPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+using PrimeAssault.Models;
+using PrimeAssault.ViewModels;
+
+namespace PrimeAssault.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+
+    /// <summary>
+    /// Index Page
+    /// </summary>
+    [DesignTimeVisible(false)]
+    public partial class MonIndexPage : ContentPage
+    {
+        // The view model, used for data binding
+        MonIndexViewModel viewModel;
+
+        /// <summary>
+        /// Constructor for Index Page
+        /// 
+        /// Get the ItemIndexView Model
+        /// </summary>
+        public MonIndexPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new MonIndexViewModel();
+        }
+        /// <summary>
+        /// The row selected from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        async void OnMonSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            MonsterModel Mon = args.SelectedItem as MonsterModel;
+            if (Mon == null)
+            {
+                return;
+            }
+
+            // Open the Read Page
+            await Navigation.PushAsync(new MonReadPage(new MonsterViewModel(Mon)));
+
+            // Manually deselect item.
+            MonListView.SelectedItem = null;
+        }
+
+        /// <summary>
+        /// Call to Add a new record
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        async void AddMon_Clicked(object sender, EventArgs e)
+        {
+
+            await Navigation.PushModalAsync(new NavigationPage(new MonCreatePage(new MonsterViewModel())));
+        }
+
+        /// <summary>
+        /// Refresh the list on page appearing
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // If no data, then reload the data
+            if (viewModel.Dataset.Count == 0)
+            {
+                viewModel.LoadDatasetCommand.Execute(null);
+            }
+        }
+    }
+}
