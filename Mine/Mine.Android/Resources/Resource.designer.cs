eccb7846e32415f8b2297b7e051f3ef5f2c6d6d2
Marco Rodriguez <48032991+Mangoslap@users.noreply.github.com>
2020-02-13 14:51:35 -0800

    Edited text colors on MonCreatePage

--- Mine/Mine.Android/Resources/Resource.designer.cs
+++ Mine/Mine.Android/Resources/Resource.designer.cs
@@ -5819,25 +5819,31 @@ namespace Mine.Droid
 			public const int notify_panel_notification_icon_bg = 2131165309;
 			
 			// aapt resource value: 0x7F07007E
-			public const int ringleader_class = 2131165310;
+			public const int raygun = 2131165310;
 			
 			// aapt resource value: 0x7F07007F
-			public const int soldier_class = 2131165311;
+			public const int ringleader_class = 2131165311;
 			
 			// aapt resource value: 0x7F070080
-			public const int TempLockedCharacter = 2131165312;
+			public const int sewer_gator = 2131165312;
 			
 			// aapt resource value: 0x7F070081
-			public const int TempProfile = 2131165313;
+			public const int soldier_class = 2131165313;
 			
 			// aapt resource value: 0x7F070082
-			public const int tooltip_frame_dark = 2131165314;
+			public const int TempLockedCharacter = 2131165314;
 			
 			// aapt resource value: 0x7F070083
-			public const int tooltip_frame_light = 2131165315;
+			public const int TempProfile = 2131165315;
 			
 			// aapt resource value: 0x7F070084
-			public const int xamarin_logo = 2131165316;
+			public const int tooltip_frame_dark = 2131165316;
+			
+			// aapt resource value: 0x7F070085
+			public const int tooltip_frame_light = 2131165317;
+			
+			// aapt resource value: 0x7F070086
+			public const int xamarin_logo = 2131165318;
 			
 			static Drawable()
 			{
